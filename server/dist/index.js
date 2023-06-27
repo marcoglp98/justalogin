84ad9c5135420f2d3a93c7ab968604f4588571ea
marcoglp98 <marcoglp98@gmail.com>
2023-06-27 18:58:25 +0200

    add registration component

--- server/dist/index.js
+++ server/dist/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 app.use(cors());
-app.post("/api/register", (req, res) => {
+app.post('/api/register', (req, res) => {
     res.json({ status: "ok" });
 });
-app.listen(3002, console.log("Server started on 3002"));
+app.listen(8000, console.log("Server started on 8000"));
--- server/src/index.ts
+++ server/src/index.ts
@@ -3,8 +3,8 @@ const app = express();
 const cors = require("cors")
 
 app.use (cors())
-app.post("/api/register", (req:any, res:any) => {
+app.post('/api/register', (req:any, res:any) => {
     res.json({status: "ok"})
 })
 
-app.listen(3002, console.log("Server started on 3002"))
+app.listen(8000, console.log("Server started on 8000"))
